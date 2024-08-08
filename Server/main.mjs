ea32f8812907c23968d8fc82b15ce8f492dade7e
1paleking1 <1paleking1@gmail.com>
2024-08-08 18:14:30 +0100

    scheduling notifications works!

--- Server/main.mjs
+++ Server/main.mjs
@@ -1,6 +1,8 @@
 import express from 'express'
-// import BodyParser from 'body-parser'
 import { Expo } from 'expo-server-sdk'
+import * as schedule from 'node-schedule';
+
+
 
 // firestore import
 import db from './firebaseConfig.mjs'
@@ -52,29 +54,52 @@ const getUsername = (email) => {
 
 }
 
-app.post('/sendNotification', async (req, res) => {
+const getNotificationDate = (days_to_wait) => {
 
-    const { admin, fridge_id, product_name, product_type } = req.body
+    return new Date(2024, 7, 8, 18, 14, 0);
+    
+    const date = new Date()
+    date.setDate(date.getDate() + days_to_wait)
+    return date
+    
+}
+
+const handleSendNotification = async (admin, fridge_id, product_name, product_type) => {
 
     const username = getUsername(admin)
-
     const message = `It's been a week since ${product_name} was added to ${username}'s fridge.`
 
     const push_tokens = await getPushTokens(fridge_id)
 
-    await console.log("push tokens obtained: ", push_tokens)
-
     for (let i = 0; i < push_tokens.length; i++) {
+            
+            expo.sendPushNotificationsAsync([{
+                to: push_tokens[i],
+                sound: 'default',
+                title: 'Fridge Inventory',
+                body: message,
+            }])
+        
+        }
 
-        await expo.sendPushNotificationsAsync([{
-            to: push_tokens[i],
-            sound: 'default',
-            title: 'Fridge Inventory',
-            body: message,
-        }])
+}
+
+
+
+
+
+
+app.post('/sendNotification', async (req, res) => {
+
+    const { admin, fridge_id, product_name, product_type } = req.body
+
+
+    const job = schedule.scheduleJob(getNotificationDate(7), async () => {
     
-    }
-
+        handleSendNotification(admin, fridge_id, product_name, product_type)
+    
+    })
+    
     res.send('Notification sent').status(200)
 
 })
@@ -83,3 +108,32 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
 
+
+
+
+// app.post('/sendNotification', async (req, res) => {
+
+//     const { admin, fridge_id, product_name, product_type } = req.body
+
+//     const username = getUsername(admin)
+
+//     const message = `It's been a week since ${product_name} was added to ${username}'s fridge.`
+
+//     const push_tokens = await getPushTokens(fridge_id)
+
+//     await console.log("push tokens obtained: ", push_tokens)
+
+//     for (let i = 0; i < push_tokens.length; i++) {
+
+//         await expo.sendPushNotificationsAsync([{
+//             to: push_tokens[i],
+//             sound: 'default',
+//             title: 'Fridge Inventory',
+//             body: message,
+//         }])
+    
+//     }
+
+//     res.send('Notification sent').status(200)
+
+// })
