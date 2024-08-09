ca5ff8727fdef7f38ae0b15ab1c6cf6d89665fc7
1paleking1 <1paleking1@gmail.com>
2024-08-09 15:39:58 +0100

    can now add and remove items from shopping list (UI)

---
+++ FridgeInventory/components/ShoppingAddModal.js
@@ -0,0 +1,85 @@
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, Modal, TextInput, TouchableWithoutFeedback, ViewBase } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Ionicons } from '@expo/vector-icons';
+
+export default function ShoppingAddModal(props) {
+
+    const [item, setItem] = useState("");
+
+    return (
+        <Modal visible={props.modalVisible} transparent={true} animationType='slide'>
+            <TouchableWithoutFeedback onPress={() => props.setModalVisible(false)}>
+                <View style={styles.ModalBackground}>
+                <TouchableWithoutFeedback>
+                    <View style={styles.ModalWindow}>
+                        
+                        <View style={styles.row}>
+                            <TextInput
+                                onChangeText={setItem}
+                                placeholder="Enter Item"
+                                style={styles.textInput}
+                            />
+
+                            <TouchableOpacity onPress={() => props.addItem(item)} style={styles.plusIcon} >
+                                <Ionicons name="add-outline" size={36} color="black" />
+                            </TouchableOpacity>
+                        </View>
+
+
+                    </View>
+                </TouchableWithoutFeedback>
+                </View>
+            </TouchableWithoutFeedback>
+        </Modal>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#eda366",
+        alignItems: "center",
+        justifyContent: "top",
+    },
+
+    ModalWindow: {
+        justifyContent: "top",
+        borderWidth: 3,
+        backgroundColor: "#ffffff",
+        padding: 10,
+        borderTopLeftRadius: 15,
+        borderTopRightRadius: 15,
+        height: '30%',
+        marginTop: 'auto',
+        width: '100%',
+        alignItems: "center",
+    },
+
+    ModalBackground: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    plusIcon: {
+
+    },
+
+    textInput: {
+        borderWidth: 1,
+        borderColor: "#000",
+        width: "80%",
+        padding: 10,
+        margin: 10,
+        borderRadius: 5,
+    },
+
+    row: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+
+});
+
--- FridgeInventory/screens/ShoppingListPage.js
+++ FridgeInventory/screens/ShoppingListPage.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { Ionicons } from '@expo/vector-icons';
 
 // component imports
+import ShoppingAddModal from "../components/ShoppingAddModal";
 
 
 
@@ -22,20 +23,18 @@ export default function ShoppingListPage({ navigation, route }) {
     }
 
     const addItem = (item) => {
-    
-        const newData = [...data, item];
+
+        const newData = [item, ...data];
         setData(newData);
     
     }
 
     return (
         <View style={styles.container}>
-
             <View style={styles.row}>
                 <Ionicons style={styles.icon} name="arrow-undo-circle-sharp" size={36} color="black" />
                 <Ionicons style={styles.icon} name="add-circle-sharp" size={36} color="black" onPress={() => setModalVisible()} />
             </View>
-
 
             <FlatList
             data={data}
@@ -48,24 +47,10 @@ export default function ShoppingListPage({ navigation, route }) {
             )}
             />
 
-            <Modal visible={modalVisible} transparent={true} animationType='slide'>
-                <View style={styles.ModalWindow}>
-
-                    <Text style={styles.title}>Add Item</Text>
-
-                    <TextInput
-                        style={{height: 40, width: 200, borderColor: 'gray', borderWidth: 1}}
-                        onChangeText={text => addItem(text)}
-                        placeholder="Enter Item"
-                    />
-
-                    <TouchableOpacity style={styles.button} onPress={() => addItem("New Item")}>
-                        <Text style={styles.buttonText}>Add Item</Text>
-                    </TouchableOpacity>
-
-                </View>
-
-            </Modal>
+            <ShoppingAddModal
+            modalVisible={modalVisible}
+            setModalVisible={setModalVisible}
+            addItem={addItem} />
 
 
         </View>
@@ -126,52 +111,6 @@ const styles = StyleSheet.create({
     icon: {
         marginHorizontal: 30,
         marginVertical: 20,
-    },
-    ModalWindow: {
-        justifyContent: "top",
-        borderWidth: 3,
-        backgroundColor: "#ffffff",
-        padding: 10,
-        // top left and top right radius is 15
-        borderTopLeftRadius: 15,
-        borderTopRightRadius: 15,
-        
-
-
-        height: '50%',
-        marginTop: 'auto',
-        width: '100%',
-        alignItems: "center",
-
-    },
-
-    ModalBackgroundOpacity: {
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-
-    Title: {
-        fontSize: 25,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-
-    CloseButton: {
-        fontSize: 20,
-        justifyContent: "center",
-        alignItems: "center",
-        paddingRight: 10,
-        flexDirection: "row",
-        alignItems: "center",
-        marginBottom: 0,
-        paddingBottom: 10,
-        paddingTop: 10,
-        // position: "relative",
-        // left:2,
-        // top: 10,
-        
     },
 
 });
